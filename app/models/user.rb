89ccca26a4d7e078a47b5817c7d4fc91beeef836
halmoallim <halmoallim@mrsool.co>
2022-02-02 14:12:39 +0300

    user validations

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,8 @@
 class User < ApplicationRecord
+    validates :user_id, presence: true
+    validates_format_of :email, with: /\A[^@\s]+@[^@\s]+\z/, presence: true
+    validates :name, presence: true, length: {maximum:30}
+    validates :password, presence: true, length: { in: 6..20 }
+
+
 end
