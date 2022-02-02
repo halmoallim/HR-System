3c582ae13829d0e1f6614b9b26c54b14df2565c5
halmoallim <halmoallim@mrsool.co>
2022-02-02 16:46:47 +0300

    association

--- app/models/division.rb
+++ app/models/division.rb
@@ -1,6 +1,15 @@
 class Division < ApplicationRecord
+    
+    #validations
     validates :name, presence: true, length: {maximum:30}
     validates :description, presence: true, length: {maximum:200}
     validates :teams, presence: true, length: {maximum:50}
     validates :manager, presence: true, length: {maximum:20}
+
+    #association
+    has_many :teams
+    belongs_to :employee
+
+
+
 end
--- app/models/target.rb
+++ app/models/target.rb
@@ -1,4 +1,6 @@
 class Target < ApplicationRecord
+
+    #validations
     validates :title, presence: true, length: {maximum:40}
     validates :description, presence: true, length: {maximum:200}
     validates :start_date, presence: true
@@ -6,5 +8,11 @@ class Target < ApplicationRecord
     validates :attribute_name, presence: true, length: {maximum:100}
     validates :team, presence: true, length: {maximum:30}
     validates :status, presence: true, length: {maximum:20}
+
+
+    #association
+    belongs_to :team
+    
+
 
 end
