32a4c91b34d9645e3c2a7115a52e0180b265156f
halmoallim <halmoallim@mrsool.co>
2022-02-14 11:22:36 +0300

    ui fix

--- app/models/team.rb
+++ app/models/team.rb
@@ -3,7 +3,6 @@ class Team < ApplicationRecord
     validates :team_id, presence: true
     validates :name, presence: true, length: {maximum:30}
     validates :description, presence: true, length: {maximum:200}
-    validates :team_lead, presence: true, length: {maximum:30}
 
     has_many :employees
     has_many :targets
